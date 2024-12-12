3970f8ec07b0d17b7e03d0b95aa0482c60030880
melrwa <orwamelkizedek@gmail.com>
2024-12-12 05:40:24 +0300

    complete lab

---
+++ app/migrations/versions/ba78c98cc628_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: ba78c98cc628
+Revises: 
+Create Date: 2024-12-12 05:21:03.948101
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ba78c98cc628'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
